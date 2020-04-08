63fa0bbfa8a417f240b265f680695cffc1c8f913
JesusD <chuy.delgado.hernandez@gmail.com>
2020-04-08 16:14:15 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,19 @@
 //              PUERTO
 // ==================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ==================================
+//              ENTORNO
+// ==================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==================================
+//              Base de datos
+// ==================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB = "mongodb+srv://JesusD84:yLtf6Af56qHte8f7@cluster0-lsxqr.mongodb.net/cafe";
+}
+process.env.URLDB = urlDB;
