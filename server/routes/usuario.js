86ecb0a5ce5987f8490096fe868ee0c5924a69ff
JesusD <chuy.delgado.hernandez@gmail.com>
2020-04-09 21:46:04 -0500

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaAdminRole } = require('../middlewares/autenticacion');
 const app = express();
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, function (req, res) {
+
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -32,7 +35,7 @@ app.get('/usuario', function (req, res) {
            });
 });
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], function (req, res) {
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -59,7 +62,7 @@ app.post('/usuario', function (req, res) {
     
 });
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre','email','img','role','estado']);
     //_.pick es paracido
@@ -81,7 +84,7 @@ app.put('/usuario/:id', function (req, res) {
     });
 });
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
     let id = req.params.id;
     let cambiaEstado = {
         estado: false
